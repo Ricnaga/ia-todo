e4c7b30b70d2d433ea6b1f4891e7f03d4b617af6
ricnaga <ric.nagato@gmail.com>
2026-09-22 15:26:42 -0300

    feat(lib): adiciona constantes de rota de login e dashboard

--- lib/constants/router-paths.ts
+++ lib/constants/router-paths.ts
@@ -1,8 +1,12 @@
 export const paths = {
   HOME: '/',
+  LOGIN: '/login',
+  REGISTER: '/register',
+  DASHBOARD: '/dashboard',
   TAREFAS: '/tarefas',
   RESUMO: '/resumo',
   BUSCA: '/busca',
+  SETTINGS: '/settings',
 } as const
 
 export type TPath = (typeof paths)[keyof typeof paths]
